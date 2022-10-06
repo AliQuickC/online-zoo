2ea9cd3d26aa25acb7139ca6144e4d3b122f13bb
Aleksandr <aleksandera9@gmail.com>
2022-10-06 23:53:52 +0300

    feat: amount panel logics

--- online-zoo/src/script/donate-scale.js
+++ online-zoo/src/script/donate-scale.js
@@ -1,11 +1,46 @@
 export default function () {
 	const donateScale = document.querySelector(".donate-progress__range");
+	const amountInput = document.querySelector(".pickandfeed-donate__another-amount");
+	const markArray = donateScale.querySelectorAll(".donate-progress__mark");
+	const amountArray = [5000, 2000, 1000, 500, 250, 100, 50, 25];
+	const amountInputMaxLength = +amountInput.dataset.maxlength;
+	const initialScaleValue = 100;
 
+	scaleInit();
+	setAmount(initialScaleValue);
+
+	// console.log(window.innerWidth);
+	// console.log(document.documentElement.clientWidth);
+
+	function scaleInit() {
+		markArray.forEach((elem, i) => {
+			elem.dataset.amount = amountArray[i];
+		});
+	}
+
+	function setAmount(amount) {
+		markArray.forEach((elem, i) => {
+			if(amountArray[i] === amount) {
+				elem.querySelector(".donate-progress__point").classList.add("donate-progress__point_active");
+				amountInput.value = amountArray[i];
+			} else {
+				elem.querySelector(".donate-progress__point").classList.remove("donate-progress__point_active");
+			}
+		});
+	}
+
+	amountInput.addEventListener("input", e => {
+		let value = e.target.value;
+		value = value.length > amountInputMaxLength ? value.substr(0, amountInputMaxLength) : value;
+		// amountArray.some(x => x === amount);
+		setAmount(+value);
+		e.target.value = value;
+	});
 
 	donateScale.onclick = (event) => {
 		const point = event.target.closest(".donate-progress__point");
-		if(point && !event.target.classList.contains("donate-progress__amount")) {
-			// console.log("!!!");
+		if(point) {
+			setAmount(+point.closest(".donate-progress__mark").dataset.amount);
 		}
 	};
 };
